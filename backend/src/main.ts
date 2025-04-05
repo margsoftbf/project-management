de57c445c70f5f03b5469ed7787a59a334417ed2
margsoftbf <mgarski@tutanota.com>
2025-04-05 13:20:50 +0200

    initial project

---
+++ backend/src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+}
+
+let server;
+async function bootstrapServer() {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init();
+    server = app.getHttpAdapter().getInstance();
+  }
+  return server;
+}
+
+export default bootstrapServer;
+
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
